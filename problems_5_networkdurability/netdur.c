6ee072aaa8cfb4cb20117cdd0cf838593967dffe
xunter <xunterrzn@gmail.com>
2020-10-02 12:30:55 +0300

    cache all from indexes, cache only if from first, use cached if no path

--- problems_5_networkdurability/netdur.c
+++ problems_5_networkdurability/netdur.c
@@ -9,18 +9,21 @@ int n = 0;
 int *nio;
 int tempi;
 
-int dist_to(int from, int to, int fromlast);
+int dist_to(int from, int to, int fromlast, int *tofromforbidden);
 int inio(int i, int j);
+int *getnio(int i, int j);
 
 int main()
 {
- scanf("%d", &n);
- nio = (int *)calloc(n * (n - 1), sizeof(int));
+ if (!scanf("%d", &n)) return -1;
+ //nio = (int *)calloc(n * (n - 1), sizeof(int));
+ nio = (int *)calloc(n * n, sizeof(int));
+ //nio = (int *)calloc(n * (int)ceil((n - 1) / 2.0), sizeof(int));
 
  for (int i = 0; i < n - 1; i++) {
   int from, to;
-  scanf("%d %d", &from, &to);
-  nio[inio(from - 1, to - 1)] = 1;
+  if (!scanf("%d %d", &from, &to)) return -1;
+  *getnio(from - 1, to - 1) = 1;
   //nio[inio(to - 1, from - 1)] = 1;
  }
 
@@ -29,18 +32,20 @@ int main()
  int durj = 0;
  for (int i = 0; i < n - 1; i++) {
   for (int j = i + 1; j < n; j++) {
-   int sumdistij = 0;
+   //printf("%d -> %d = %d\n", i + 1, j + 1, *getnio(i, j));
+   int sumdistij = n;
    for (int k = 0; k < n; k++) {
     if (k == i || k == j) continue;
   //printf("dist %d->%d?\n", k + 1, i + 1);
-    int disti = dist_to(k, i, -1);
+    int disti = dist_to(k, i, -1, 0);
   //printf("dist %d->%d: %d\n", k + 1, i + 1, disti);
 
   //printf("dist %d->%d?\n", k + 1, j + 1);
-    int distj = dist_to(k, j, -1);
+    int distj = dist_to(k, j, -1, 0);
   //printf("dist %d->%d: %d\n", k + 1, j + 1, distj);
 
-    sumdistij += disti + distj;
+    int distijmin = disti < distj ? disti : distj;
+    if (sumdistij == n || sumdistij < distijmin) sumdistij = distijmin;
    }
    if (maxdist == -1 || sumdistij < maxdist) {
     maxdist = sumdistij;
@@ -56,32 +61,57 @@ int main()
  return 0;
 }
 
-int dist_to(int from, int to, int fromlast) {
+int dist_to(int from, int to, int fromlast, int *tofromforbidden) {
  if (from == to) return 0;
  if (from > to) {
   tempi = from;
   from = to;
   to = tempi;
  }
- int distknown = nio[inio(from, to)];
- if (distknown > 0 && distknown < n) {
-  //printf("dist %d->%d: %d (known)\n", from + 1, to + 1, distknown);
-  return nio[inio(from, to)];
+ int dist = n;
+ if (*getnio(from, to) > 0) {
+  //printf("dist %d->%d: %d (known)\n", from + 1, to + 1, *getnio(from, to));
+  //printf("dist %d->%d: %d (known)\n", from + 1, to + 1, *getnio(from, to));
+  dist = *getnio(from, to);
  } else {
   int mindist = n - 1;
   for (int i = 0; i < n; i++) {
-   if (i == fromlast || i == from || i == to) continue;
-   if (nio[inio(from, i)] != 1) continue;
-   int disti = dist_to(i, to, from);
+   if (i == fromlast || i == from || i == to || fromlast != -1 && tofromforbidden[i]) continue;
+   if (*getnio(from, i) != 1) continue;
+   if (fromlast == -1) {
+    tofromforbidden = (int *)calloc(n, sizeof(int));
+   }
+   tofromforbidden[from] = 1;
+   int disti = dist_to(i, to, fromlast == -1 ? from : fromlast, tofromforbidden);
    if (disti < mindist) mindist = disti;
    if (disti == 1) break;
   }
-  int dist = 1 + mindist;
-  nio[inio(from, to)] = dist;
+  dist = 1 + mindist;
+  if (fromlast == -1) *getnio(from, to) = dist;
   //nio[inio(to, from)] = dist;
   //printf("dist %d->%d: %d\n", from + 1, to + 1, dist);
-  return dist;
  }
+ if (fromlast == -1 && tofromforbidden != 0) {
+  free(tofromforbidden);
+ }
+ return dist;
+}
+
+int *getnio(int i, int j) {
+ if (i > j) {
+  tempi = i;
+  i = j;
+  j = tempi;
+ }
+ //int in = i * n + j;
+ /*
+ if (i >= (int)ceil((n - 1) / 2.0)) {
+  int isub = n % 2 == 0 ? 1 : 2;
+  in = i * (n - isub - i) + (n - 1 - j);
+ }
+ printf("[%d][%d]=%d = %d ceil: %d\n", i, j, in, nio[in], (int)ceil((n - 1) / 2.0));
+*/
+ return nio + i * n + j;
 }
 
 int inio(int i, int j) {
@@ -90,10 +120,13 @@ int inio(int i, int j) {
   i = j;
   j = tempi;
  }
+ int in = i * n + j;
  /*
- if (i > ceil((n - 1) / 2)) {
-  return i * (n - 1 - i) + (n - 1 - j);
+ if (i >= (int)ceil((n - 1) / 2.0)) {
+  int isub = n % 2 == 0 ? 1 : 2;
+  in = i * (n - isub - i) + (n - 1 - j);
  }
- */
- return i * n + j;
+ printf("[%d][%d]=%d = %d ceil: %d\n", i, j, in, nio[in], (int)ceil((n - 1) / 2.0));
+*/
+ return in;
 }
